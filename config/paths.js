8778f1bc69f00397e247e957ebd98e55a290deb8
wangtengyz <752132694@qq.com>
2019-08-19 00:19:33 +0800

    11

--- config/paths.js
+++ config/paths.js
@@ -5,13 +5,14 @@ const fs = require('fs');
 const url = require('url');
 // 引入 globby 模块
 const globby = require('globby');
-// 入口文件路径
-const entriesPath = globby.sync([resolveApp('src') + '/*/index.js']);
+
 
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebook/create-react-app/issues/637
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+// 入口文件路径
+const entriesPath = globby.sync([resolveApp('src') + '/*/index.js'], {cwd: process.cwd()});
 
 const envPublicUrl = process.env.PUBLIC_URL;
 
--- scripts/start.js
+++ scripts/start.js
@@ -1,5 +1,6 @@
 'use strict';
-
+// Ensure environment variables are read.
+require('../config/env');
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
@@ -11,8 +12,7 @@ process.on('unhandledRejection', err => {
   throw err;
 });
 
-// Ensure environment variables are read.
-require('../config/env');
+
 
 
 const fs = require('fs');
